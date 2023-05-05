acf00925199d45a5ad934721d970a47bb5c9eb12
xiayuxuan <2562833605@qq.com>
2023-05-05 10:34:17 +0800

    查询

--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -52,3 +52,32 @@ BookInfo.objects.get(id=5).delete()
 BookInfo.objects.filter(id=5).delete()
 
 
+# 查询
+# 查询单一结果，如果不存在则会抛出异常
+try:
+    book = BookInfo.objects.get(id=1)
+except Exception as e:
+    print("查询结果不存在")
+# all 查询多个结果
+BookInfo.objects.all()
+# count 查询结果数量
+BookInfo.objects.all().count()
+BookInfo.objects.count()
+
+# 过滤查询 实现sql 的where功能
+# filter过滤多个结果
+# exclude排除符合条件的剩下的结果
+# get过滤单一结果
+
+# 模型类名.objects.filter(属性名__运算符=值)
+# 模型类名.objects.exclude(属性名__运算符=值)
+# 模型类名.objects.get(属性名__运算符=值) 简写：（属性名=值）
+BookInfo.objects.get(id__exact=1)
+BookInfo.objects.get(id=1)
+BookInfo.objects.filter(id=1)
+BookInfo.objects.get(pk=1)
+
+
+
+
+

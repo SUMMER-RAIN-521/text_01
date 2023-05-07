6f9c407455cb7911a8522547713085a8fead3e59
xiayuxuan <2562833605@qq.com>
2023-05-07 10:48:27 +0800

    json和form

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from book.views import create_book
+from book.views import create_book, shop, register, json, method
 
 urlpatterns = [
     path('create/', create_book),
+    # <转换器的名字：变量名> 转换器会对变量名进行正则验证
+    path('<int:city_id>/<int:shop_id>', shop),
+    path('register/', register),
+    path('json/', json),
+    path('method/', method)
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -1,14 +1,64 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from book.models import BookInfo
+
 # Create your views here.
 
 
+# url以？分割为两部分，后面一部分为查询字符串，多个数据以&拼接
 def create_book(request):
     book = BookInfo.objects.create(
         name='abc',
         pub_date='2001-9-4',
         readcount=10
     )
-
     return HttpResponse('create')
+
+
+def shop(request, city_id, shop_id):
+
+    query_params = request.GET
+    # print(query_params)
+    oder = query_params.getlist('key')
+    print(oder)
+
+    print(city_id, shop_id)
+    return HttpResponse('起火啦！')
+
+
+def register(request):
+
+    data = request.POST
+    print(data)
+
+    return HttpResponse('ok')
+
+
+def json(request):
+    body = request.body
+    body_str = body.decode()
+    print(body_str) # 字符串
+    print(type(body_str))
+    # json的字符串可以转换为python的字典
+    import json
+    body_dict = json.loads(body_str)
+    print(body_dict)
+    # 请求头的数据
+    # print(request.META)
+    print(request.META['SERVER_PORT'])
+    return HttpResponse('json')
+
+
+def method(request):
+
+    print(request.method)
+    return  HttpResponse('method')
+
+
+
+
+
+
+
+
+
